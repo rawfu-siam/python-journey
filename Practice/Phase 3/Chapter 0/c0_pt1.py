f2c6a4bd9f8f540650107c75cd404088e197a0e2
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-05 15:05:39 +0600

    Added practice tasks for chapter 0 topic 1 of phase 3

---
+++ Practice/Phase 3/Chapter 0/c0_pt1.py
@@ -0,0 +1,20 @@
+'''
+Chapter0, topic - python -m venv env — creating virtual environments
+'''
+'''
+Task 1 (Easy): Open your terminal, create a fresh directory called 
+agency_test, move inside it, and successfully generate an environment 
+named dev_env.
+'''
+# DONE in other folder in my pc
+'''
+Task 2 (Medium): Create a virtual environment named sec_env and write a 
+manual text file named .gitignore containing the text line to block it.
+'''
+# DONE 
+'''
+Task 3 (Hard): Look inside your generated environment folder. Locate the 
+file named pyvenv.cfg, open it inside VS Code, and read the text line that 
+shows the home path of your system's original Python installer.
+'''
+# DONE 
