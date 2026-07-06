946745ce8d593747d3a93d713a2bf4c2351c5b60
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-06 14:14:13 +0600

    Added practice tasks for chapter 0 topic 4

---
+++ Practice/Phase 3/Chapter 0/c0_pt4.py
@@ -0,0 +1,20 @@
+'''
+Chapter0, topic - pip freeze > requirements.txt
+'''
+'''
+Task 1 (Easy): Make sure your environment is activated, install any small package 
+(like secure), and type pip freeze to watch its name pop up on screen with its 
+version number.
+'''
+# DONE 
+'''
+Task 2 (Medium): Run the full pip freeze > requirements.txt command, verify a new 
+file pops into your project sidebar, and click to open it.
+'''
+# DONE 
+'''
+Task 3 (Hard): Intentionally install one more package (like tqdm), rerun your freeze 
+command, and look inside your requirements.txt file to confirm that the text updated 
+itself dynamically without manual typing.
+'''
+# DONE 
