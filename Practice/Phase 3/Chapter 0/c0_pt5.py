eab5de7845f1c2aada2427d74ff8abd721b95c1d
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-06 18:24:34 +0600

    Added practice tasks for chapter 0 topic 5

---
+++ Practice/Phase 3/Chapter 0/c0_pt5.py
@@ -0,0 +1,20 @@
+'''
+Chapter0, topic - pip install -r requirements.txt
+'''
+'''
+Task 1 (Easy): Create a brand-new folder, make a fresh env, and activate it. 
+Manually create a text file called requirements.txt and write down a single line: 
+secure==0.3.0.
+'''
+# DONE 
+'''
+Task 2 (Medium): Run the bulk installation command on your terminal to watch pip read 
+your manual text file and automatically download the item.
+'''
+# DONE 
+'''
+Task 3 (Hard): Run a pip list step to confirm it arrived, then delete your whole env 
+folder entirely, recreate it, and rerun your -r installation script to watch your 
+environment recover its packages flawlessly from scratch!
+'''
+# DONE 
