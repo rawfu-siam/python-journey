902441fbc8868344288cd1ea2659f3fc0f0305b1
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-06 13:04:17 +0600

    Added practice tasks for chapter 0 topic 3

---
+++ Practice/Phase 3/Chapter 0/c0_pt3.py
@@ -0,0 +1,19 @@
+'''
+Chapter0, topic - pip install, pip uninstall, pip list
+'''
+'''
+Task 1 (Easy): Make sure your environment is activated, then use your delivery 
+driver to install a package called colorama.
+'''
+# DONE 
+'''
+Task 2 (Medium): Run your inventory check to confirm colorama is sitting inside 
+your list, and write down the exact version number it gave you.
+'''
+# colorama 0.4.6 
+# DONE 
+'''
+Task 3 (Hard): Successfully uninstall colorama, and run an inventory check right 
+after to prove your project closet is back to its original clean state.
+'''
+# DONE 
