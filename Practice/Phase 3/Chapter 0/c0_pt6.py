a2e9200ea712e07d2ae06ec7624511c018718014
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-07 12:58:06 +0600

    Added practice tasks for chapter 0 topic 6

---
+++ Practice/Phase 3/Chapter 0/c0_pt6.py
@@ -0,0 +1,20 @@
+'''
+Chapter0, topic - .gitignore — never commit env/
+'''
+'''
+Task 1 (Easy): Create a brand-new folder, make a blank text file named exactly 
+.gitignore, and add a line to block a folder called env/.
+'''
+# DONE 
+'''
+Task 2 (Medium): Add a professional code comment inside your .gitignore file 
+using the correct symbol to explain to a future teammate why the environment 
+folder is blocked.
+'''
+# DONE 
+'''
+Task 3 (Hard): Look up how to use your terminal to check your active Git status 
+(git status). Run it to confirm that once your .gitignore file is filled out, 
+your local env/ folder completely vanishes from Git's untracked file alert list.
+'''
+# DONE 
