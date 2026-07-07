8b3140cf04e76f065b408e8e5803f3a262bf042e
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-08 03:54:02 +0600

    Added practice tasks for chapter 0 topic 7

---
+++ Practice/Phase 3/Chapter 0/c0_pt7.py
@@ -0,0 +1,22 @@
+'''
+Chapter0, topic - why global installs cause conflicts
+'''
+'''
+Task 1 (Easy): Open your terminal without activating any environment and run 
+pip list. Count how many tools are installed globally on your machine.
+'''
+# DONE 
+'''
+Task 2 (Medium): Activate your project's custom env folder, run pip list, 
+and compare how much cleaner and shorter your private sandbox inventory list 
+is compared to the global list.
+'''
+# DONE 
+'''
+Task 3 (Hard): Write a simple Python file called test_env.py that imports a 
+package you only installed inside your virtual environment (like secure). 
+Try running it while the environment is active, then deactivate the 
+environment and try running it again to watch Python throw a clean 
+ModuleNotFoundError.
+'''
+# DONE 
