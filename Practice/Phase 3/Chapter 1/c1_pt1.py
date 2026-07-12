7b54542e989cb1ef55d79b2e915eccca0340a22d
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-12 23:02:56 +0600

    Added practice tasks for chapter 1 topic 1

---
+++ Practice/Phase 3/Chapter 1/c1_pt1.py
@@ -0,0 +1,19 @@
+'''
+Chapter1, topic - git init, clone, add, commit, push, pull
+'''
+'''
+Task 1 (Easy): Initialize a brand new local repository inside a practice folder named git_camp, 
+create a blank file named app.py, and run git status to see Git flag your file in red as "untracked".
+'''
+# DONE 
+'''
+Task 2 (Medium): Stage your new file using the add command, run git status again to see it glow 
+green as "staged", and run your first commit with a professional message label.
+'''
+# DONE 
+'''
+Task 3 (Hard): Create a free account on github.com if you haven't yet. Create a blank private remote 
+repository online, follow the quick copy-paste commands on your screen to connect your local 
+terminal repo to it, and successfully run git push to upload your app.py script to the web!
+'''
+# DONE
