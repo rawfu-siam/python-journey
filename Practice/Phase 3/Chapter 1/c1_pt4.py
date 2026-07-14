d8917d05ebb1af882edb5e6229997b282ff89294
rawfu-siam <rawfusiam2006@gmail.com>
2026-07-15 03:55:45 +0600

    Added practice tasks for chapter 1 topic 4

---
+++ Practice/Phase 3/Chapter 1/c1_pt4.py
@@ -0,0 +1,23 @@
+'''
+Chapter1, topic - git stash, git log, git diff
+'''
+'''
+Task 1 (Easy): Open an existing local repository directory folder, modify a text 
+sentence in any file, and run git diff to watch your terminal format your 
+modifications in red and green syntax text rows.
+'''
+# DONE 
+'''
+Task 2 (Medium): Run git log --oneline on your terminal screen workspace, spot 
+your very first initial checkpoint ID string, and type out its first 4 tracking 
+characters.
+'''
+# de0f 
+# DONE 
+'''
+Task 3 (Hard): Make a messy uncommitted edit, type git stash to watch your edit 
+magically disappear from your VS Code file window, verify your directory is 
+100% clean, and then run git stash pop to make your code text pop straight 
+back into existence!
+'''
+# DONE 
